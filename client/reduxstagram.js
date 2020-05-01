4bce70dc0b80c04334ec163d730ce40adde73b90
BingShawLim <shaw.a.muvee@gmail.com>
2020-04-30 23:50:43 -0500

    created components folder and  js files

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,19 +1,8 @@
 import React, { Component } from 'react';
 import {render} from 'react-dom';
-
 import css from './styles/style.styl';
-import { renderIntoDocument } from 'react-addons-test-utils';
 
-export class reduxstagram extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
-}
+import Main from './components/Main';
 
-export default reduxstagram
 
-render(<p>Hi</p>, document.getElementById('root'));
+render(<Main/>, document.getElementById('root'));

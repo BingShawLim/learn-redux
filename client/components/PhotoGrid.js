6148474031a42ed3acbfb7ede586be2f92cfff50
BingShawLim <shaw.a.muvee@gmail.com>
2020-05-01 11:53:36 -0500

    initial setup components

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router'
+
+export class PhotoGrid extends Component {
+    render() {
+        return (
+            <div className="photo-grid">
+                
+            </div>
+        )
+    }
+}
+
+export default PhotoGrid

d525c39081028fc9c2402648918c9f9eeb5dca27
BingShawLim <shaw.a.muvee@gmail.com>
2020-05-03 15:10:04 -0500

    building single photo page, added comment.js.

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import Photo from './Photo';
+import Comment from './Comments'
+
 import {Link} from 'react-router'
 
 export class Single extends Component {
+
     render() {
+        const i = this.props.posts.findIndex( (post) => post.code === this.props.params.postId)
+        const post = this.props.posts[i]
+
         return (
             <div className="single-photo">
-                <p>single</p>
+                <Photo i={i} post={post} {...this.props}/>
+                <Comment />
             </div>
         )
     }

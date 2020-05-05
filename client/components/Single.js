1902f9d4fca2124ffe60eff68ed2e2b763de4ae8
BingShawLim <shaw.a.muvee@gmail.com>
2020-05-04 22:29:56 -0500

    building comments.js to display comments.

--- client/components/Single.js
+++ client/components/Single.js
@@ -8,13 +8,16 @@ import {Link} from 'react-router'
 export class Single extends Component {
 
     render() {
-        const i = this.props.posts.findIndex( (post) => post.code === this.props.params.postId)
-        const post = this.props.posts[i]
+        const {postId} = this.props.params;
+        const i = this.props.posts.findIndex( (post) => post.code === postId);
+        const post = this.props.posts[i];
+
+        const postComments = this.props.comments[postId] || [];
 
         return (
             <div className="single-photo">
                 <Photo i={i} post={post} {...this.props}/>
-                <Comment />
+                <Comment postComments={postComments} {...this.props}/>
             </div>
         )
     }
